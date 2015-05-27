6a9ca0d3f94daf3df9b112141daea134f3e87eba
jesseDJ <jesse_dj@live.com>
2015-05-27 12:11:50 -0400

    haha

---
+++ Class4validation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Class4validation.Global" Language="C#" %>
---
+++ Class4validation/default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Class4validation
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            //calculate tip
+            Decimal tip = Convert.ToDecimal(txtAmount.Text) * Convert.ToDecimal (ddlPercent.SelectedValue);
+
+            //display tip
+            lblTip.Text = tip.ToString("c");//c in quotes formats as currency, overload
+
+            //calculate total
+            Decimal total = Convert.ToDecimal(txtAmount.Text) + tip;
+
+            //display total
+            lblTotal.Text = total.ToString("c");
+
+            //display the results
+            pnlResults.Visible = true;
+        }
+
+    }
+}
